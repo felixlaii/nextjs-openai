ee88e978c72a26d97883e2221343b106ff2da1e0
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-04 20:46:55 -0500

    imported useState and useEffect and put input tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   return (
     <>
       <h1>ChatGPT Clone</h1>
+      <form>
+        <input type="text" placeholder="type your message" />
+      </form>
     </>
   );
 }
