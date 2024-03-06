39c94ff37b5770e300af99894673f691f4601a6b
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-06 18:39:14 -0500

    added sonner and create useReponse

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
+import { useChat } from "ai/react";
 
 import { useClerk, useUser } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 export default function Chat() {
   const [subscriptionDialogOpen, setSubscriptionDialogOpen] = useState(false);
@@ -11,5 +13,35 @@ export default function Chat() {
   const credits = user?.publicMetadata?.credits;
   const newUser = typeof credits === "undefined";
   const paidUser = user?.publicMetadata?.stripeCustomerId;
+
+  const ref = useRef<HTMLDivElement>(null);
+  const { messages, input, handleInputChange, handleSubmit, isLoading, error } =
+    useChat({
+      initialMessages: [
+        {
+          id: Date.now().toString(),
+          role: "system",
+          content: "You are an assistant that gives short answers.",
+        },
+      ],
+      onResponse: (response: any) => {
+        if (!response.ok) {
+          const status = response.status;
+
+          switch (status) {
+            case 401:
+              openSignIn();
+              break;
+            case 402:
+              toast.error("You have no credits left.", {
+                action: {
+                  label: "Get more",
+                  onClick: () => setSubscriptionDialogOpen(true),
+                },
+              });
+          }
+        }
+      },
+    });
   return <div></div>;
 }
