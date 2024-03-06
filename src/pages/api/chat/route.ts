da18c0667909216e187290312ff05be2976c5248
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-06 18:12:46 -0500

    Update route.ts

--- src/pages/api/chat/route.ts
+++ src/pages/api/chat/route.ts
@@ -2,3 +2,6 @@ import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import { NextResponse } from "next/server";
 import { clerkClient, currentUser } from "@clerk/nextjs";
+
+export const runtime = "edge";
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
