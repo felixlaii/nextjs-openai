8ba19734d8d78dbd0f634b042e685eb47aba6e48
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-06 18:33:41 -0500

    useUser and useClerk

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 
+import { useClerk, useUser } from "@clerk/nextjs";
+
 export default function Chat() {
   const [subscriptionDialogOpen, setSubscriptionDialogOpen] = useState(false);
+
+  const { isLoaded, isSignedIn, user } = useUser();
+  const { openSignIn, session } = useClerk();
   return <div></div>;
 }
