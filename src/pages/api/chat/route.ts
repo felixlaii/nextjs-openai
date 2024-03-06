ac0b6cd359b199f6db8ca0a2f3711b36b8d5c458
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-06 18:18:54 -0500

    Update route.ts

--- src/pages/api/chat/route.ts
+++ src/pages/api/chat/route.ts
@@ -9,13 +9,39 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
 export async function POST(req: Request) {
   try {
     if (!process.env.OPENAI_API_KEY) {
-      return new NextResponse('Missing OpenAI API Key.', { status: 400 })
+      return new NextResponse("Missing OpenAI API Key.", { status: 400 });
     }
 
-    const user = await currentUser()
+    const user = await currentUser();
 
     if (!user) {
-      return new NextResponse('You need to sign in first.', {status: 401})
+      return new NextResponse("You need to sign in first.", { status: 401 });
     }
+
+    const credits = Number(user.publicMetadata?.credits || 0);
+
+    if (!credits) {
+      return new NextResponse("You have no credits left.", { status: 402 });
+    }
+
+    const { messages } = await req.json();
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages,
+    });
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+      publicMetadata: {
+        credits: credits - 1,
+      },
+    });
+
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error: any) {
+    return new NextResponse(error.message || "Something went wrong", {
+      status: 500,
+    });
   }
 }
