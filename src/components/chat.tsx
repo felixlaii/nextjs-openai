832abeb64f736c2cc612b5d6732cf75a070aaff1
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-06 18:33:05 -0500

    useState for subscription dialog

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function Chat() {
+  const [subscriptionDialogOpen, setSubscriptionDialogOpen] = useState(false);
   return <div></div>;
 }
