61c4c9622c2eee60d17beada619342c2e7f30a61
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-09 16:18:05 -0500

    imported my nextjs boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <div></div>;
+}
