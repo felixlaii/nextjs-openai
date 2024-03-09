0e4cc5c596754eafb8fb8034226cfc77a2b2cc46
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-09 16:43:34 -0500

    created handleInput

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,11 @@ export default function Home() {
   const [value, setValue] = useState<string>("");
   const [assistantMessage, setAssistantMessage] = useState<string | null>(null);
 
+  const handleInput = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    },
+    []
+  );
   return <div></div>;
 }
