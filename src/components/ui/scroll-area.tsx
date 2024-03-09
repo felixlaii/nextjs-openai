7905dbed10a7cd261b50914d623a7e66ae65d5c1
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-09 17:39:31 -0500

    Update scroll-area.tsx

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,3 +1,21 @@
 import React from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 import { cn } from "../../lib/utils";
+
+const ScrollArea = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
+  <ScrollAreaPrimitive.Root
+    className={cn("relative overflow-hidden", className)}
+    {...props}
+  >
+    <ScrollAreaPrimitive.Viewport
+      className="h-full w-full rounded-[inherit]"
+      ref={ref}
+    >
+      {children}
+    </ScrollAreaPrimitive.Viewport>
+  </ScrollAreaPrimitive.Root>
+));
+export { ScrollArea, ScrollBar };
