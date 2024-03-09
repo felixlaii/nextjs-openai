dbdd4c6cd952604674328f93267ae7a110dd7100
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-09 16:42:46 -0500

    deleted import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return <div></div>;
