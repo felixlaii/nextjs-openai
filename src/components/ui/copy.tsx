e43bb4135913b74a5b87398e3cf9cca4813aedc8
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-12 22:10:09 -0400

    added dependency and imports

--- src/components/ui/copy.tsx
+++ src/components/ui/copy.tsx
@@ -1,5 +1,15 @@
 import { type Message } from "ai";
 
+import { useClipboard } from "@custom-react-hooks/all";
+
 interface ChatMessageActionsProps extends React.ComponentProps<"div"> {
   message: Message;
+}
+
+export default function Copy({
+  message,
+  className,
+  ...props
+}: ChatMessageActionsProps) {
+  const { copyToClipboard, pasteFromClipboard, state } = useClipboard();
 }
