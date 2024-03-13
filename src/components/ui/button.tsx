bc1c3cc675a271ffa98e3996268dbaef134e6436
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-03-12 21:25:07 -0400

    added size for button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,12 @@ const buttonVariants = cva(
           "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
       },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
     },
   }
 );
